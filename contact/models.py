c5ed1fc858040e1124e5b7cb140eb0d48b60fbec
DevHaro <developerharo@gmail.com>
2021-03-21 21:52:32 -0600

    Creating Contact Model

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
-class Contact():
 
+class Contact(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  phone = models.CharField(max_length=20, blank=True)
+  email = models.EmailField(blank=True)
+  address = models.TextField(blank=True)
 
+  def __str__(self):
+      return f"{self.last_name}, {self.first_name}"
